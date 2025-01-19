8f8f0aa6f528059bdc65a81c0721857d9a20bb7c
Diego Mariano <diogohenks@hotmail.com>
2025-01-19 18:41:31 -0300

    Update template.php

--- app/Views/template.php
+++ app/Views/template.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="pt-br">
 <head>
-    <title>COCaDA-DB</title>
+    <title>COCaDA</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
